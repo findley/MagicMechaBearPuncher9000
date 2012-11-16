5cf7194e3a2bd8f6042c38d78b482e9dcb0b03d9
msalvato <msalvato@mit.edu>
2012-11-16 15:46:05 -0500

    oops

---
+++ src/dudes/Player.java
@@ -0,0 +1,19 @@
+package dudes;
+
+import java.util.HashMap;
+
+public abstract class Player extends Dude{
+	HashMap<String, Integer> buttons;
+	
+	void move(String button){
+		//something to do with modifying loc based on button
+	}
+	
+	void pickup(){
+		// tries to pick up what might be nearby.
+	}
+	
+	public float[] weaponLoc(){
+		return new float[] {pos[0]+16, pos[1] + 6};
+	}
+}
