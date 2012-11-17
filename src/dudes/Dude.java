73145a48987b4b2c0bf5edaeaccea7fb3b286f9a
Law Smith <sonokin@mit.edu>
2012-11-16 23:46:29 -0500

    Dudes now have health bars.

--- src/dudes/Dude.java
+++ src/dudes/Dude.java
@@ -1,5 +1,6 @@
 package dudes;
 
+import org.newdawn.slick.Color;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
@@ -15,6 +16,8 @@ public abstract class Dude {
 	public boolean isRight;
 	public boolean isAttacking;
 	public float moveSpeed;
+	public int maxHealth;
+	public Color healthFill;
 	
 	public abstract float[] weaponLoc();
 	
@@ -28,5 +31,17 @@ public abstract class Dude {
 	
 	public void render(Graphics g) throws SlickException {
 		new Image("Assets/npcs/daughterFront.png").draw(pos[0], pos[1], 2);
+		
+		//Render a health bar for the Dude
+				int offset = -10;
+				float x = pos[0];
+				float y = pos[1]+offset;
+				int width = 100;
+				int height = 10;
+				int padding = 2;
+				int healthRemaining = width*health/maxHealth;
+				g.drawRect(x-padding-width/2, y-padding-height/2, width+padding*2, height+padding*2);
+				g.setColor(healthFill);
+				g.fillRect(x-width/2, y-height/2, healthRemaining, height);
 	}
 }
