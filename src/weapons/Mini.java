ca89d289d633524942ea3992a286e600a42b71fc
dheins <daheins@gmail.com>
2012-11-28 15:16:35 -0500

    Forgot images

---
+++ src/weapons/Mini.java
@@ -0,0 +1,63 @@
+package weapons;
+
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.SpriteSheet;
+import org.newdawn.slick.geom.Rectangle;
+
+import dudes.Dude;
+import dudes.Player;
+
+public class Mini extends Weapon {
+
+	public Mini(Dude owner) {
+		this(owner.pos[0], owner.pos[1]);
+		assignOwner(owner);
+	}
+	
+	public Mini(float x, float y) {
+		super();
+		this.x = x;
+		this.y = y;
+		damage = 5;
+		attackWidth = 30;
+		attackHeight = 6;
+		attackTime = 200;
+		delayTime = 500;
+		playerSize = 32;
+	}
+
+	@Override
+	public void init() throws SlickException {
+		weaponSheet = new SpriteSheet("Assets/Weapons/Mini/player" + ((Player)owner).playerID + "Mini.png", playerSize, playerSize);
+		playerSheet = ((Player)owner).sprites;
+		defaultSprite[0] = weaponSheet.getSprite(0, 5);
+		defaultSprite[1] = weaponSheet.getSprite(0, 4);
+		initAnimations();
+	}
+	
+	@Override
+	public void attack() {
+		float[] corner = owner.weaponLoc();
+		corner[0] -= attackWidth / 2;
+		corner[1] -= attackHeight / 2;
+		Rectangle hitbox = new Rectangle(corner[0], corner[1], attackWidth,
+				attackHeight);
+		attacks.add(new Attack(owner.isRight, hitbox, "player"));
+	}
+
+	@Override
+	protected boolean updateAttack(Attack attack) {
+		if(owner.isAttacking) {
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	@Override
+	public void createGroundSprite() throws SlickException {
+		groundSprite = new Image("Assets/Weapons/Mini/miniSprite.png");
+	}
+}
