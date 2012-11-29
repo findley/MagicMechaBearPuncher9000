96a09b04bb4022ce8a21ff45b8e9219f701ea5ce
neastin <neastin@mit.edu>
2012-11-29 14:48:17 -0500

    made tigher fireball img

--- src/projectiles/FireballProjectile.java
+++ src/projectiles/FireballProjectile.java
@@ -15,9 +15,9 @@ public class FireballProjectile extends Projectile{
 		this.moveSpeed = 5.0f;
 		this.damage = 10;
 		if (isRight) {
-			this.sprite = new Image("Assets/Weapons/Fireball/RightFireball.png");
+			this.sprite = new Image("Assets/Weapons/Fireball/RightFireball2.png");
 		} else {
-			this.sprite = new Image("Assets/Weapons/Fireball/LeftFireball.png");
+			this.sprite = new Image("Assets/Weapons/Fireball/LeftFireball2.png");
 		}
 		this.hitbox = new Rectangle(pos[0], pos[1], sprite.getWidth(), sprite.getHeight());
 	}
